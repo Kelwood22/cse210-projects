bc8d0cf256aa142c4812e11e431ab3f8df7a1c89
kelwood22 <kwoodland@byupathway.edu>
2025-05-08 12:28:06 -0600

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string letterGrade = string.Empty;
+        string plusOrMinus = string.Empty;
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (grade % 10 >= 7)
+        {
+            plusOrMinus = "+";
+        }
+        else if (grade % 10 <= 3)
+        {
+            plusOrMinus = "-";
+        }
+        else
+        {
+            plusOrMinus = string.Empty;
+        }
+
+        if (grade >= 97 || grade <= 60)
+        {
+            plusOrMinus = string.Empty; // No plus or minus for A+
+        }
+        
+        Console.WriteLine($"Your letter grade is: {letterGrade}{plusOrMinus}");
+       
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class. Try again!");
+        }
     }
 }
