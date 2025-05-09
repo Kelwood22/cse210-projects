dc66cec4f7cd644e0b7f9e89721a7707a16a4c2c
kelwood22 <kwoodland@byupathway.edu>
2025-05-08 22:03:10 -0600

    Exercise 3.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); 
+
+        int guess = -1;
+        int attempts = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it! You took {attempts} attempts.");
+                Console.WriteLine("Do you want to play again? (yes/no)");
+                string response = Console.ReadLine().ToLower();
+                if (response == "yes")
+                {
+                    magicNumber = randomGenerator.Next(1, 101); 
+                    guess = -1;
+                    attempts = 0; 
+                }
+                else
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    break;
+                }
+            }
+        }  
     }
 }
